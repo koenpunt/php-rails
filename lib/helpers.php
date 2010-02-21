53c01111c1ac9e7ff61c1eed6f19170323f024df
Tom Rochette <roctom@gmail.com>
2010-02-21 15:58:55 -0500

    Added t helper.

--- lib/helpers.php
+++ lib/helpers.php
@@ -1,10 +1,17 @@
 <?php
 
+use I18n\I18n;
 use I18n\Symbol;
+
+function t($key, $options = array())
+{
+	return I18n::translate($key, $options);
+}
 
 function _s($value = null)
 {
 	return new Symbol($value);
 }
+
 
 ?>
