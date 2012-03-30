8e5313096eaf06c6254433c75df39332cb970ad0
Koen Punt <koenpunt@gmail.com>
2012-03-30 15:48:27 +0200

    uuid now generates valid v4 uuid

--- ruby/lib/RSecureRandom.php
+++ ruby/lib/RSecureRandom.php
@@ -214,8 +214,8 @@ class RSecureRandom{
 	#
 	public static function uuid(){
 		$ary = unpack("v*", self::random_bytes(16));
-		$ary[2] = ($ary[2] & 0x0fff) | 0x4000;
-		$ary[3] = ($ary[3] & 0x3fff) | 0x8000;
-		return vsprintf("%08x-%04x-%04x-%04x-%04x%08x", $ary);
+		$ary[4] = $ary[4] & 0x0fff | 0x4000;
+		$ary[5] = $ary[5] & 0x3fff | 0x8000;
+		return vsprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x', $ary);
 	}
 }
