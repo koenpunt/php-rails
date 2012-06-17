f2a8f9fb587769418dbbe9b5b8a9763ef0fc1ecb
Koen Punt <koenpunt@gmail.com>
2012-06-17 17:22:50 +0200

    Removed autoloader

--- PHPRails.php
+++ PHPRails.php
@@ -4,8 +4,8 @@ if (!defined('PHP_VERSION_ID') || PHP_VERSION_ID < 50300)
 	
 define('PHP_RAILS_VERSION_ID','0.2');
 
-if (!defined('PHP_RAILS_AUTOLOAD_PREPEND'))
-	define('PHP_RAILS_AUTOLOAD_PREPEND',true);
+#if (!defined('PHP_RAILS_AUTOLOAD_PREPEND'))
+#	define('PHP_RAILS_AUTOLOAD_PREPEND',true);
 
 if(!defined('PS'))
 	define('PS', PATH_SEPARATOR);
@@ -16,10 +16,10 @@ if(!defined('DS'))
 
 require __DIR__ . DS . 'utils.php';
 
-if (!defined('PHP_RAILS_AUTOLOAD_DISABLE')){
-	spl_autoload_register(array('PHPRails', 'load'), false, PHP_RAILS_AUTOLOAD_PREPEND);
-}
-#PHPRails::uses('action_view/helpers/tag_helper');
+#if (!defined('PHP_RAILS_AUTOLOAD_DISABLE')){
+#	spl_autoload_register(array('PHPRails', 'load'), false, PHP_RAILS_AUTOLOAD_PREPEND);
+#}
+
 class PHPRails{
 	
 	static $current = array();
@@ -35,67 +35,21 @@ class PHPRails{
 			'active_support' => __DIR__ . DS . 'activesupport' . DS . 'lib',
 			'action_pack' => __DIR__ . DS . 'actionpack' . DS . 'lib',
 			'action_view' => __DIR__ . DS . 'actionpack' . DS . 'lib',
-			'helper' => __DIR__ . DS . 'actionpack' . DS . 'lib' . DS . 'action_view' . DS . 'helpers',
 			'ruby' => __DIR__ . DS . 'ruby' . DS . 'lib' ,
 		);
 	}
 	
-	public static function load($className){
-		$mapClassName = str_replace('\\', '-', $className);
-		
-		if( !isset(self::$_classMap[$mapClassName]) ){
-			return false;
-		}
-		
-		$package = self::$_classMap[$mapClassName];
-			
-		if( $file = self::_mapped($mapClassName) ){
-			return include $file;
-		}
-		
-		$path = self::path($package);
-		#var_dump(self::$_classMap, $path);
-		if( $namespaces = self::_get_namespaces($className) ){
-			$className = array_pop($namespaces);
-			#var_dump($className);
-			$file = $path . DS . $className . '.php';
-			if(file_exists($file)){
-				self::_map($file, $mapClassName);
-				return include $file;
-			}
+	public static function import($path){
+#		if ($file = self::_mapped($path)) {
+#			return include $file;
+#		}
+		$file = self::path($path) . '.php';
+		if(file_exists($file)){
+			self::_map($path, $file);
+			return require_once $file;
 		}
 	}
 	
-	public static function uses($class_or_path_with_class, $path = null){
-		if(is_null($path)){
-			$last_slash = strrpos($class_or_path_with_class, '/');
-			$path = $last_slash ? substr($class_or_path_with_class, 0, $last_slash) : $class_or_path_with_class;
-			$class = str_replace(' ', '', ucwords(str_replace('_', ' ', ( $last_slash ? substr($class_or_path_with_class, $last_slash + 1) : $class_or_path_with_class ))));
-		}else{
-			$class = $class_or_path_with_class;
-			
-		}
-		$mapClassName = preg_replace_callback('/(\/)?(\w+)/', function($match){
-			return ($match[1] ? '-' : '') . preg_replace('/([a-z]+)_?/e', "ucfirst('\\1')", $match[2]);
-		} , $path);
-		
-		self::$_classMap[$mapClassName . '-' . $class] = $path;
-	}
-	
-	/**
-	 * Used to read information stored path
-	 *
-	 * Usage:
-	 *
-	 * `App::path('Model'); will return all paths for models`
-	 *
-	 * `App::path('Model/Datasource', 'MyPlugin'); will return the path for datasources under the 'MyPlugin' plugin`
-	 *
-	 * @param string $type type of path
-	 * @param string $plugin name of plugin
-	 * @return array
-	 * @link http://book.cakephp.org/2.0/en/core-utility-libraries/app.html#App::path
-	 */
 	public static function path($location) {
 		if($slash = strpos($location, '/')){
 			$type = substr($location, 0, $slash);
@@ -111,16 +65,8 @@ class PHPRails{
 		}, self::$_packages[$type]);
 	}
 	
-	/**
-	 * Maps the $name to the $file.
-	 *
-	 * @param string $file full path to file
-	 * @param string $name unique name for this map
-	 * @param string $plugin camelized if object is from a plugin, the name of the plugin
-	 * @return void
-	 */
-	protected static function _map($file, $name) {
-		self::$_map[$name] = $file;
+	protected static function _map($path, $file) {
+		self::$_map[$path] = $file;
 	}
 	
 	/**
